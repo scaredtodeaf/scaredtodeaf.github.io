19691480a0326738712541a2dd59f9c0a36c8261
Shelby Kira Marvell <shelby@marvell.cat>
2022-01-16 19:48:21 -0800

    expanding another way

--- scripts.js
+++ scripts.js
@@ -52,9 +52,5 @@ for (let pre of pres)
 
 function toggle_visibility(id) {
     event.preventDefault();
-    var e = document.getElementById(id);
-    if(e.style.display == 'block')
-       e.style.display = 'none';
-    else
-       e.style.display = 'block';
+    this.classList.toggle('expanded');
  }
