d459b5bbe89d3798e5d6dd77abeaf57cc22087a3
wheelsfire <keltconor@gmail.com>
2022-02-26 10:45:24 +1100

    Fixes

---
+++ js/music_player.js
@@ -0,0 +1,174 @@
+var AUD_OBJ = new Audio('media/0000000.mp3'); // Audio Object
+var AUD_CTM; // Audio - Track (Current Time)
+var AUD_RCT; // Audio - Track (Current Time | Raw)
+var AUD_DUR; // Audio - Track (Duration)
+var AUD_RDR; // Audio - Track (Duration | Raw)
+var AUD_VLP = 100; // Audio - Volume Percentage
+var BTN_CKI = new Audio('media/sfx/button_pressin.ogg'); // SFX - Button (Click In)
+var BTN_CKO = new Audio('media/sfx/button_pressout.ogg'); // SFX - Button (Click Out)
+BTN_CKO.volume = 0.8; // Volume Level - BTN_CKO
+BTN_CKI.volume = 0.8; // Volume Level - BTN_CKI
+
+// Convert the time from the Audio Object, into a Readable Format.
+function TRACK_FRT(TIME_DATA){
+	if (AUD_OBJ.duration > 0 && AUD_OBJ.duration < 3600){
+		return FRT_DTE = new Date(TIME_DATA * 1000).toISOString().substr(14, 5);
+	} else if (AUD_OBJ.duration > 3600 && AUD_OBJ.duration < 86400){
+		return FRT_DTE = new Date(TIME_DATA * 1000).toISOString().substr(11, 8);
+	};
+};
+
+// Anything that needs to be repeatedly executed, goes here.
+function REP_EXEC(){
+	AUD_RCT = AUD_OBJ.currentTime; // Update the Raw Current Time, for precise checks.
+	AUD_RDR = AUD_OBJ.duration; // Update the Raw Duration, for precise checks.
+	AUD_CTM = TRACK_FRT(AUD_OBJ.currentTime); // Put the Current Time into a Readable Format.
+	AUD_DUR = TRACK_FRT(AUD_OBJ.duration); // Put the Track Duration into a Readable Format.
+	AUD_VLP = Math.round(AUD_OBJ.volume * 100); // Turn the Volume Amount used by JavaScript into a Readable Format (Percentage).
+	
+	var AUD_CBT = '';
+	if (AUD_CTM != null && AUD_DUR != null){
+		AUD_CBT = AUD_CBT.concat(AUD_CTM, ' / ', AUD_DUR);
+	} else {
+		AUD_CBT = '00:00 / 00:00';
+	};
+
+	$('#MP_MSI').html(AUD_CBT); // Flash the 'MP_MSI' div with the updated data.
+	
+	// Check if the track is paused, but finished.
+	if (AUD_OBJ.paused == true && AUD_OBJ.currentTime == AUD_OBJ.duration){
+		AUD_OBJ.currentTime = 0; // The track has finished, reset to the beginning.
+	};
+	
+	// Change the Icon for the Play Button, based on if the music is playing or not.
+	if (AUD_OBJ.paused == true){
+		document.getElementById('MP_PPB').src = 'img/play.png';
+		document.getElementById('MP_PPB').alt = 'Play';
+		document.getElementById('MP_PPB').title = 'Play';
+	} else if (AUD_OBJ.paused == false){
+		document.getElementById('MP_PPB').src = 'img/pause.png';
+		document.getElementById('MP_PPB').alt = 'Pause';
+		document.getElementById('MP_PPB').title = 'Pause';
+	};
+	
+	if (AUD_OBJ.muted == true){
+		document.getElementById('MP_MUMB').alt = 'Muted';
+		document.getElementById('MP_MUMB').title = 'Muted';
+		
+		if (AUD_OBJ.volume >= 0 && AUD_OBJ.volume <= 0.3399999999999999){
+			document.getElementById('MP_MUMB').src = 'img/mute_1.png';
+		} else if (AUD_OBJ.volume >= 0.34 && AUD_OBJ.volume <= 0.66999999999999999){
+			document.getElementById('MP_MUMB').src = 'img/mute_2.png';
+		} else if (AUD_OBJ.volume >= 0.67 && AUD_OBJ.volume <= 1){
+			document.getElementById('MP_MUMB').src = 'img/mute_3.png';
+		};
+	} else if (AUD_OBJ.muted == false){
+		var AUD_APT = '';
+		AUD_APT = AUD_APT.concat(AUD_VLP, '%');
+		document.getElementById('MP_MUMB').alt = AUD_APT;
+		document.getElementById('MP_MUMB').title = AUD_APT;
+		
+		if (AUD_OBJ.volume >= 0 && AUD_OBJ.volume <= 0.33){
+			document.getElementById('MP_MUMB').src = 'img/volume_1.png';
+		} else if (AUD_OBJ.volume >= 0.34 && AUD_OBJ.volume <= 0.66){
+			document.getElementById('MP_MUMB').src = 'img/volume_2.png';
+		} else if (AUD_OBJ.volume >= 0.67 && AUD_OBJ.volume <= 1){
+			document.getElementById('MP_MUMB').src ='img/volume_3.png';
+		};
+	};
+};
+
+// Load a Track into Player, and correct variables.
+function LOAD_TRACK(TRACK_ID){
+	var CCT_LNK = ''; // Concatenated Link to Load.
+	if (AUD_OBJ.paused == false){
+		AUD_OBJ.pause();
+	};
+	
+	AUD_OBJ.currentTime = 0; // Put the Current Time back to 0.
+	CCT_LNK = CCT_LNK.concat('media/', TRACK_ID, '.mp3'); // Concatenate the TRACK_ID into a Readable Link.
+	AUD_OBJ.src = CCT_LNK; // Load that file into the Audio Object.
+	AUD_OBJ.load(); // Reload the Audio Object, to refresh properties.
+};
+
+// Everything to be done once the page is ready.
+$(document).ready(function(){
+	
+	// Request that the REP_EXEC() function is called, every 100ms.
+	repExec = setInterval('REP_EXEC()', 100);
+	
+	// Change the Play / Pause Stop Icon's contrast depending on whether the mouse is or isn't over it.
+	$(function (){
+		$('#MP_PP').hover(function(){
+			document.getElementById('MP_PPB').style.filter = 'contrast(50%)';
+		}, function (){
+			document.getElementById('MP_PPB').style.filter = 'contrast(100%)';
+		});
+	});
+	
+	// Play / Pause the Player
+	$('#MP_PP').click(function(){
+		if (AUD_OBJ.paused == true){
+			BTN_CKO.play();
+			AUD_OBJ.play();
+			
+		} else if (AUD_OBJ.paused == false){
+			BTN_CKI.play();
+			AUD_OBJ.pause();
+		};
+	});
+	
+	// Change the Stop Icon's contrast depending on whether the mouse is or isn't over it.
+	$(function (){
+		$('#MP_STP').hover(function(){
+			document.getElementById('MP_STPB').style.filter = 'contrast(50%)';
+		}, function (){
+			document.getElementById('MP_STPB').style.filter = 'contrast(100%)';
+		});
+	});
+	
+	// Stop the Player
+	$('#MP_STP').click(function(){
+		BTN_CKI.play();
+
+		if (AUD_OBJ.paused == false){
+			AUD_OBJ.pause();
+		};
+		
+		AUD_OBJ.currentTime = 0;
+	});
+	
+	// Change the Mute / Unmute Icon's contrast depending on whether the mouse is or isn't over it,
+	// Change the volume level based on scroll direction.
+	$(function (){
+		$('#MP_MUM').hover(function(){
+			document.getElementById('MP_MUMB').style.filter = 'contrast(50%)';
+			
+			document.getElementById('MP_MUMB').addEventListener('wheel', function(event){
+				if (event.deltaY < 0){
+					if (AUD_OBJ.volume < 1){
+						AUD_OBJ.volume += 0.01;
+					};
+				} else if (event.deltaY > 0){
+					if (AUD_OBJ.volume > 0){
+						AUD_OBJ.volume -= 0.01;
+					};
+				};
+			});
+		}, function (){
+			document.getElementById('MP_MUMB').style.filter = 'contrast(100%)';
+		});
+	});
+	
+	// Mute / Unmute the Player.
+	$('#MP_MUM').click(function(){
+		if (AUD_OBJ.muted == true){
+			BTN_CKO.play();
+			AUD_OBJ.muted = false;
+			
+		} else if (AUD_OBJ.muted == false){
+			BTN_CKI.play();
+			AUD_OBJ.muted = true;
+		};
+	});
+});
---
+++ js/quiz_logic.js
@@ -0,0 +1,243 @@
+var QUIZ_DATA = new Array(); // The Quiz Data
+var TID; // This ID
+var ETID; // Edited TID
+var LTID; // Last TID
+var CUR_ANSE; // Current Answer - Edited
+var CUR_ANSR; // Current Answer - Raw
+var FF_CLIP = new Audio('media/9999999.mp3'); // Music Track
+var TYP_CLI = new Audio('media/SFX/typewriter_click.ogg'); // SFX - Typewriter (Click)
+var TYP_CLA = new Audio('media/SFX/typewriter_clack.ogg'); // SFX - Typewriter (Clack)
+var TYP_DIN = new Audio('media/SFX/typewriter_ding.ogg'); // SFX - Typewriter (Ding)
+var BTN_CLK = new Audio('media/SFX/button_click.ogg'); // SFX - Button Click
+var RGT_ANS = new Audio('media/SFX/right_answer.ogg'); // SFX - Right Answer
+var RNG_ANS = new Audio('media/SFX/wrong_answer.ogg'); // SFX - Wrong Answer
+var HNT_KPR = new Audio('media/SFX/hint_keeper.ogg'); // SFX - Phantasmagoria's Hint Keeper Line
+HNT_KPR.volume = 0.8; // Volume Level - HNT_KPR
+RNG_ANS.volume = 0.6; // Volume Level - RNG_ANS
+RGT_ANS.volume = 0.6; // Volume Level - RGT_ANS
+BTN_CLK.volume = 0.8; // Volume Level - BTN_CLK
+TYP_DIN.volune = 0.8; // Volume Level - TYP_DIN
+TYP_CLA.volume = 0.8; // Volume Level - TYP_CLA
+TYP_CLI.volume = 0.8; // Volume Level - TYP_CLI
+FF_CLIP.volume = 1; // Volume Level - FF_CLIP
+
+// Create Div 'Buttons' from the Quiz Data and append them into the 'BTN_C' Div. 
+function MAKE_DIVS_FROM_QDATA(){
+	for(var i=0; i<QUIZ_DATA.length; i++){
+		var TOTAL_COUNT = i+1;
+		$('#BTN_C').append('<div class="BTN_I" isClickable=true hintUnlocked=false hintClicked=false TID=' + TOTAL_COUNT + '>' + TOTAL_COUNT + '</div>');
+	};
+};
+
+// Function to Retrieve a json and return the resulting content.
+async function LOAD_AND_PARSE_QDATA(JSON_FILE){
+	var FTCH_FAILED = false;
+	var JSON_DATA = await fetch(JSON_FILE).then(response => response.json().then(data => {return data;})).catch(FTCH_FAILED = true);
+	
+	// Code to make sure we don't continue until we get the JSON_DATA or FTCH_FAILED is true.
+	while (JSON_DATA === null && FTCH_FAILED === false){};
+
+	if (JSON_DATA != null){
+		var STRING_FROM_JSON = JSON.stringify(JSON_DATA);
+		var ARRAY_FROM_JSON = JSON.parse(STRING_FROM_JSON);
+		
+		for(var i = 0; i<ARRAY_FROM_JSON.length; i++){
+			QUIZ_DATA[i] = new Array (
+				ARRAY_FROM_JSON[i]['ID'], 
+				ARRAY_FROM_JSON[i]['Name'], 
+				ARRAY_FROM_JSON[i]['Title'], 
+				ARRAY_FROM_JSON[i]['Hint'], 
+				ARRAY_FROM_JSON[i]['Keywords']
+			);
+		};
+
+		MAKE_DIVS_FROM_QDATA();
+	} else {
+		return false;
+	};
+};
+
+// Prepare a string for comparison with another, by stripping certain characters and converting to lowercase.
+function PREP_FOR_COMPARE(STRING, MODE){
+	if (MODE == 0){
+		// Get rid of all characters which are non-alphanumeric (including spaces).
+		STRING = STRING.replace(/[^0-9A-z]+/g, '');
+	} else if (MODE == 1){
+		// Same as above, but excluding (){}|[], as they can be potentially used for
+		// Powerful Keywords, e.g: "(Sid Meier's) [[Civ(ilization)] {IV|4}".
+		STRING = STRING.replace(/[^0-9A-z(){}|[\]]+/g, '');
+	}; 
+	
+	// Convert the String to Lower Case
+	STRING = STRING.toLowerCase();
+	
+	// Return the results.
+	return STRING;
+};
+
+// Parse and return the Game's Name and Track.
+function GEN_GAMEINFO(){
+	var GAM_INF = ''; // Game Info
+	
+	if (QUIZ_DATA[ETID][1].length > 0 && QUIZ_DATA[ETID][2].length > 0){ // If both items have information...
+		// ...Concatenate them both together with a hyphen between them.
+		GAM_INF += QUIZ_DATA[ETID][1]; GAM_INF += ' - '; GAM_INF += QUIZ_DATA[ETID][2];
+	} else if (QUIZ_DATA[ETID][1].length > 0 && QUIZ_DATA[ETID][2].length == 0){ // If only the first item has information...
+		// ...Only return the game name as the Game Info.
+		GAM_INF += QUIZ_DATA[ETID][1];
+	};
+	
+	// Return this information to whatever called it.
+	return GAM_INF;
+}
+
+// Anything that needs to be repeatedly executed, goes here.
+function REP_CHECK(){
+	// Code to make sure the Game Area is always the height of the page, minus the Header Area and is positioned correctly.
+	var GA_HEIGHT = $(window).height() - $('#HDR').height(); // Game Area Height: (Page Height - Header Height)
+	$('#GA').css('height', GA_HEIGHT + 'px'); // Set Game Area Height.
+	$('#GA').css('top', $('#HDR').height() + 'px'); // Set Game Area Position.
+	
+	if (TID != null){
+		if ((LTID != TID) && LTID != null){
+			if ($('.BTN_I:eq(' + (LTID-1) + ')').attr('isClickable') == 'true'){
+				if ($('.BTN_I:eq(' + (LTID-1) + ')').attr('isClickable') == 'true'){
+					if($('.BTN_I:eq(' + (LTID-1) + ')').css('backgroundColor') == 'rgb(30, 229, 123)'){
+						$('.BTN_I:eq(' + (LTID-1) + ')').css('backgroundColor', '#1e88e5');
+					};
+				};
+			};
+		};
+
+		LTID = TID;
+
+		if ($('.BTN_I:eq(' + ETID + ')').attr('hintUnlocked') == 'true'){
+			if ($('.BTN_I:eq(' + ETID + ')').attr('hintClicked') == 'true'){
+				$('#HB').html(QUIZ_DATA[ETID][3]);
+			} else if ($('.BTN_I:eq(' + ETID + ')').attr('hintClicked') == 'false'){
+				$('#HB').html('You can now click for a hint!');
+			};
+
+		} else if ($('.BTN_I:eq(' + ETID + ')').attr('hintUnlocked') == 'false'){
+			var HB_STRING = '';
+			var TRK_SL = Math.round(AUD_RCT - (AUD_RDR / 2));
+			if (TRK_SL < 0){TRK_SL = Math.abs(TRK_SL);};
+
+			if (AUD_OBJ.paused != true){
+				if (isNaN(TRK_SL) != true){
+					HB_STRING = HB_STRING.concat('You can click here for a hint in roughly ', TRK_SL, ' seconds.');
+					$('#HB').html(HB_STRING);
+					
+					if (TRK_SL == 0){$('.BTN_I:eq(' + ETID + ')').attr('hintUnlocked', true);};
+				};
+			};
+		};
+		
+		var ANS_ARRAY = QUIZ_DATA[ETID][4];
+		for (var i=0; i<ANS_ARRAY.length; i++){ANS_ARRAY[i] = PREP_FOR_COMPARE(ANS_ARRAY[i], 1);};
+		
+		CUR_ANSE = PREP_FOR_COMPARE($('#MP_ANSR').val(), 0);
+		CUR_ANSR = $('#MP_ANSR').val();
+		
+		if ($.inArray(CUR_ANSE, ANS_ARRAY) >= 0){
+			$('#MP_ANSR').attr('disabled', true);
+			$('#MP_ANSR').css('backgroundColor', '#e5991e');
+			$('#MP_ANSR').val('');
+			RGT_ANS.play();
+			AUD_OBJ.pause();
+			
+			$('.BTN_I:eq(' + ETID + ')').css('backgroundColor', '#e5991e');
+			$('.BTN_I:eq(' + ETID + ')').css('font-size', '8pt');
+			$('.BTN_I:eq(' + ETID + ')').text(GEN_GAMEINFO());
+			$('.BTN_I:eq(' + ETID + ')').attr('isClickable', false);
+			
+			setTimeout(function(){$('#MP_ANSR').css('backgroundColor', '#1e88e5');}, 1000);
+		};
+	} else if (TID == null){
+		$('#MP_ANSR').attr('disabled', false);
+		$('#HB').html('I am the official hintkeeper, I watch your every move! Ask me for a hint if you are hopelessly stuck, but use me sparingly; Too many hints can spoil the game!');
+
+		var CHECK_LIST = ['Freddy Pharkas'];
+		for (var i=0; i<CHECK_LIST.length; i++){CHECK_LIST[i] = PREP_FOR_COMPARE(CHECK_LIST[i], 1);};
+
+		CUR_ANSE = PREP_FOR_COMPARE($('#MP_ANSR').val(), 0);
+		CUR_ANSR = $('#MP_ANSR').val();
+
+		if ($.inArray(CUR_ANSE, CHECK_LIST) >= 0){
+			$('#MP_ANSR').css('backgroundColor', '#e5991e');
+			$('#MP_ANSR').val('');
+			RGT_ANS.play();
+			AUD_OBJ.pause();
+			
+			setTimeout(function(){$('#MP_ANSR').css('backgroundColor', '#1e88e5'); FF_CLIP.play();}, 1000);
+		};
+	};
+};
+
+// Everything to be done once the page is ready.
+$(document).ready(function(){
+	// Call this function.
+	LOAD_AND_PARSE_QDATA('game_data.json');
+	
+	// Request that the REP_CHECK() function is called, every 100ms.
+	repCheck = setInterval('REP_CHECK()', 100);
+
+	// Check if a button on the user's keyboard has been pressed.
+	$(document).keydown(function(event){
+		if ($('#MP_ANSR').is(':focus')){
+			// Any readable character.
+			if ((event.which > 47 && event.which < 58) || (event.which> 64 && event.which < 91) || (event.which > 95 && event.which < 112) || (event.which > 185 && event.which < 193) || (event.which > 218 && event.which < 223)){TYP_CLI.play();};
+			
+			// Space Bar
+			if (event.which === 32){TYP_CLA.play();};
+
+			// Enter Key
+			if (event.which === 13){TYP_DIN.play();};
+		};
+	});
+
+	// Check if a button on the user's keyboard has been lifted.
+	$(document).keyup(function(event){
+		if ($('#MP_ANSR').is(':focus')){
+			if (event.which === 13){
+				$('#MP_ANSR').attr('disabled', true);
+				$('#MP_ANSR').css('backgroundColor', '#e5351e');
+				$('#MP_ANSR').val('');
+				RNG_ANS.play();
+
+				setTimeout(function(){$('#MP_ANSR').css('backgroundColor', '#1e88e5'); $('#MP_ANSR').focus();}, 1300);
+			};
+		};
+	});
+	
+	$('#BTN_C').on('click','.BTN_I', function(event){
+		if ($(this).attr('isClickable') == 'true'){
+			$('#MP_ANSR').attr('disabled', false);
+			$(this).css('backgroundColor', '#1ee57b');
+			
+			TID = $(this).attr('TID');
+			ETID = TID - 1;
+			LOAD_TRACK(QUIZ_DATA[ETID][0]);
+			
+			BTN_CLK.play();
+			setTimeout(function(){AUD_OBJ.play(); $('#MP_ANSR').focus();}, 450);
+		} else {
+			LOAD_TRACK(QUIZ_DATA[($(this).attr('TID'))-1][0]);
+		};
+    });
+	
+	$('#HB').click(function(event){
+		if ($(this).attr('isClickable') == 'true'){
+			if (TID != null){
+				if ($('.BTN_I:eq(' + ETID + ')').attr('hintUnlocked') == 'true'){
+					if ($('.BTN_I:eq(' + ETID + ')').attr('hintClicked') == 'false'){
+						$('.BTN_I:eq(' + ETID + ')').attr('hintClicked', true);
+						$('#HB').html(QUIZ_DATA[ETID][3]);
+					};
+				};
+			} else {
+				HNT_KPR.play();
+			};
+		};
+	});
+});
