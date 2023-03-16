0f24de6ea2e4e11fe4cdc89c311e3f8640048339
scaredtodeaf <keltconor@gmail.com>
2023-03-16 18:23:07 +1100

    Lots of Work

--- VideoQuiz/script.js
+++ VideoQuiz/script.js
@@ -1,20 +1,57 @@
-// Get a reference to the video player and the button grid
 const videoPlayer = document.getElementById('video-player');
-const buttonGrid = document.getElementById('button-grid');
+const videoButtons = document.getElementById('button-grid');
+const answerInput = document.getElementById('answer-input');
+const answerFeedback = document.getElementById('answer-feedback');
 
-// Load the JSON file
+let currentVideoIndex = 0;
+let currentButton;
+
+// Load the video data from the JSON file
 fetch('quiz-data.json')
-    .then(response => response.json())
-    .then(data => {
-        // Create a button for each video in the JSON data
-        data.forEach(video => {
-            const button = document.createElement('button');
-            button.textContent = video.Name;
-            button.addEventListener('click', () => {
-                // Set the source of the video player to the selected video
-                videoPlayer.src = `QuizVideos/${video.ID}.webm`;
-                videoPlayer.load();
-            });
-            buttonGrid.appendChild(button);
-        });
+  .then(response => response.json())
+  .then(data => {
+    // Set up the video player with the first video
+    const firstVideo = data[0];
+    videoPlayer.src = `QuizVideos/${firstVideo.ID}.webm`;
+
+    // Set up the video buttons
+    for (let i = 0; i < data.length; i++) {
+      const video = data[i];
+      const button = document.createElement('button');
+      button.innerText = video.Name;
+      button.dataset.index = i;
+      button.addEventListener('click', () => {
+        currentVideoIndex = i;
+        currentButton = button;
+        videoPlayer.src = `QuizVideos/${video.ID}.webm`;
+        answerInput.disabled = false;
+        answerInput.value = '';
+        answerFeedback.innerText = '';
+      });
+      videoButtons.appendChild(button);
+    }
+
+    currentButton = videoButtons.querySelector(`button[data-index="${currentVideoIndex}"]`);
+    currentButton.classList.add('current-video');
+
+    // Set up the answer input
+    answerInput.addEventListener('keyup', () => {
+      const inputValue = answerInput.value.trim().toLowerCase();
+      let isMatch = false;
+      for (const keyword of data[currentVideoIndex].Keywords) {
+        if (inputValue === keyword.toLowerCase()) {
+          isMatch = true;
+          break;
+        }
+      }
+      if (isMatch) {
+        answerFeedback.innerText = 'Correct!';
+        answerFeedback.style.color = 'green';
+        currentButton.classList.add('correct-answer');
+        answerInput.disabled = true;
+      } else {
+        answerFeedback.innerText = '';
+      }
     });
+  })
+  .catch(error => console.error(error));
