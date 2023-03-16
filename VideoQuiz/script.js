446fa3c69810c453b8adaac16e9086c1616d3e38
scaredtodeaf <keltconor@gmail.com>
2023-03-16 15:22:07 +1100

    Slow Fix

--- VideoQuiz/script.js
+++ VideoQuiz/script.js
@@ -1,66 +1,20 @@
-const videoSelect = document.getElementById('videoSelect');
-const myVideo = document.getElementById('myVideo');
-const playBtn = document.getElementById('playBtn');
-const hintBtn = document.getElementById('hintBtn');
-const hintDiv = document.getElementById('hintDiv');
-const answerInput = document.getElementById('answerInput');
-const myForm = document.getElementById('myForm');
+// Get a reference to the video player and the button grid
+const videoPlayer = document.getElementById('video-player');
+const buttonGrid = document.getElementById('button-grid');
 
-// Load the JSON data
-fetch('quiz-data.json')
-  .then(response => response.json())
-  .then(data => {
-    // Populate the video options
-    data.videos.forEach((video, index) => {
-      const option = document.createElement('option');
-      option.value = index;
-      option.textContent = video.src;
-      videoSelect.appendChild(option);
+// Load the JSON file
+fetch('videos.json')
+    .then(response => response.json())
+    .then(data => {
+        // Create a button for each video in the JSON data
+        data.forEach(video => {
+            const button = document.createElement('button');
+            button.textContent = video.Name;
+            button.addEventListener('click', () => {
+                // Set the source of the video player to the selected video
+                videoPlayer.src = `QuizVideos/${video.ID}.webm`;
+                videoPlayer.load();
+            });
+            buttonGrid.appendChild(button);
+        });
     });
-
-    // Set the default video
-    myVideo.src = `QuizVideos/${data.videos[0].src}.mp4`;
-
-    // Set the answers and hint for the current video
-    let currentAnswers = data.videos[0].answers;
-    let currentHint = data.videos[0].hint;
-
-    // Update the answers and hint when the user selects a different video
-    videoSelect.addEventListener('change', () => {
-      const selectedIndex = videoSelect.selectedIndex;
-      myVideo.src = `QuizVideos/${data.videos[selectedIndex].src}.mp4`;
-      currentAnswers = data.videos[selectedIndex].answers;
-      currentHint = data.videos[selectedIndex].hint;
-      hintBtn.disabled = true;
-      hintDiv.style.display = 'none';
-      myForm.reset();
-    });
-
-    // Enable the hint button when the video reaches the halfway point
-    myVideo.addEventListener('timeupdate', () => {
-      if (myVideo.currentTime >= myVideo.duration / 2) {
-        hintBtn.disabled = false;
-      }
-    });
-
-    // Display the hint when the user clicks the hint button
-    hintBtn.addEventListener('click', () => {
-      hintDiv.textContent = currentHint;
-      hintDiv.style.display = 'block';
-    });
-
-    // Handle form submission
-    myForm.addEventListener('submit', event => {
-      event.preventDefault();
-      const userAnswer = answerInput.value.trim().toLowerCase();
-      const isCorrect = currentAnswers.some(answer => answer.toLowerCase() === userAnswer);
-      if (isCorrect) {
-        alert('Correct!');
-      } else {
-        alert('Incorrect!');
-      }
-      myForm.reset();
-      hintDiv.style.display = 'none';
-      hintBtn.disabled = true;
-    });
-  });
