efe5d2c2223ae370eebb007b79b3f6c0699c5fa6
scaredtodeaf <keltconor@gmail.com>
2023-03-18 01:45:52 +1100

    Quick Fix

--- VideoQuiz/script.js
+++ VideoQuiz/script.js
@@ -93,7 +93,7 @@ fetch('quiz-data.json')
         }
       }
       if (isMatch) {
-        answerFeedback.innerText = 'Correct!';
+        answerFeedback.innerText = `Correct! The game was ${video.Name} !`;
         answerFeedback.style.color = 'green';
         answerInput.disabled = true;
         const buttons = document.querySelectorAll('#button-grid button');
