8d417a654edf85f8cdf9b8f87b67235b0f662b6d
scaredtodeaf <keltconor@gmail.com>
2023-03-17 21:55:20 +1100

    Fixing CSS

--- VideoQuiz/script.js
+++ VideoQuiz/script.js
@@ -17,47 +17,52 @@ fetch('quiz-data.json')
     videoPlayer.src = `QuizVideos/${video.ID}.webm`;
 
     // Set up the video buttons
-    for (const [index, videoData] of data.entries()) { // Use entries() to get the index
-      const button = document.createElement('button');
-      button.innerText = index + 1; // Set the button text as the index + 1
-      button.videoData = videoData;
-      button.hintAvailable = false;
-      button.hintShown = false;
-      button.currentHint = '';
-      button.addEventListener('click', () => {
-        video = videoData;
-        videoPlayer.src = `QuizVideos/${video.ID}.webm`;
-        answerInput.disabled = false;
-        answerInput.value = '';
-        answerFeedback.innerText = '';
-
-        // Update hint status and hint text based on the button's state
-        hintAvailable = button.hintAvailable;
-        hintShown = button.hintShown;
-        currentHint = button.currentHint;
-
-        if (hintAvailable && !hintShown) {
-          answerFeedback.innerText = 'Hint Available - Click here';
-          answerFeedback.style.color = 'black';
-        } else if (hintShown) {
-          answerFeedback.innerText = `Hint: ${currentHint}`;
-          answerFeedback.style.color = 'red';
-        }
-
-        const buttons = document.querySelectorAll('#button-grid button');
-        for (const button of buttons) {
-          button.classList.remove('correct-answer');
-          button.disabled = false;
-        }
-      });
-      videoButtons.appendChild(button);
-    }
+    for (const [index, videoData] of data.entries()) {
+        const button = document.createElement('button');
+        button.innerText = index + 1;
+        button.videoData = videoData;
+        button.hintAvailable = false;
+        button.hintShown = false;
+        button.currentHint = '';
+        button.addEventListener('click', () => {
+          video = videoData;
+          videoPlayer.src = `QuizVideos/${video.ID}.webm`;
+          answerInput.disabled = false;
+          answerInput.value = '';
+          answerFeedback.innerText = '';
+  
+          hintAvailable = button.hintAvailable;
+          hintShown = button.hintShown;
+          currentHint = button.currentHint;
+  
+          if (hintAvailable && !hintShown) {
+            hintLabel.innerText = 'Hint Available - Click here';
+            hintLabel.style.color = 'black';
+            hintLabel.addEventListener('click', showHint);
+          } else if (hintShown) {
+            hintLabel.innerText = `Hint: ${currentHint}`;
+            hintLabel.style.color = 'red';
+          } else {
+            hintLabel.innerText = '';
+            hintLabel.style.color = 'black';
+            hintLabel.removeEventListener('click', showHint);
+          }
+  
+          const buttons = document.querySelectorAll('#button-grid button');
+          for (const button of buttons) {
+            button.classList.remove('correct-answer');
+            button.disabled = false;
+          }
+        });
+        videoButtons.appendChild(button);
+      }
 
     // ...
 
 
-    // Set up the answer input
-    answerInput.addEventListener('keyup', () => {
+    const answerForm = document.getElementById('answer-form');
+    answerForm.addEventListener('submit', (event) => {
+      event.preventDefault();
       const inputValue = answerInput.value.trim().toLowerCase();
       let isMatch = false;
       for (const keyword of video.Keywords) {
@@ -75,6 +80,7 @@ fetch('quiz-data.json')
           if (button.videoData === video) {
             button.classList.add('correct-answer');
             button.disabled = true;
+            button.style.backgroundColor = 'green';
           }
         }
         hintAvailable = false;
@@ -86,7 +92,7 @@ fetch('quiz-data.json')
     });
 
     // Set up the hint display
-    let currentButton = null; // Add this line to store the current button element
+    const hintLabel = document.getElementById('hint');
     videoPlayer.addEventListener('timeupdate', () => {
       const halfDuration = videoPlayer.duration / 2;
       if (videoPlayer.currentTime > halfDuration && !hintAvailable && !hintShown) {
@@ -94,26 +100,22 @@ fetch('quiz-data.json')
         const buttons = document.querySelectorAll('#button-grid button');
         for (const button of buttons) {
           if (button.videoData === video) {
-            currentButton = button; // Update the currentButton variable
             button.hintAvailable = true;
             button.addEventListener('click', showHint);
           }
         }
-        answerFeedback.innerText = 'Hint Available - Click here';
-        answerFeedback.style.color = 'black';
-        answerFeedback.addEventListener('click', showHint);
+        hintLabel.innerText = 'Hint Available - Click here';
+        hintLabel.style.color = 'black';
+        hintLabel.addEventListener('click', showHint);
       }
     });
-
+    
     // Function to show the hint
     function showHint() {
-        if (!currentButton.hintShown) { // Use currentButton instead of this
-          if (currentButton.currentHint === '') { // Use currentButton instead of this
-            currentButton.currentHint = video.Hint; // Use currentButton instead of this
-          }
-          answerFeedback.innerText = `Hint: ${currentButton.currentHint}`; // Use currentButton instead of this
-          answerFeedback.style.color = 'red';
-          currentButton.hintShown = true; // Use currentButton instead of this
-        }
-      };
+      if (!hintShown) {
+        hintShown = true;
+        hintLabel.innerText = `Hint: ${video.Hint}`;
+        hintLabel.style.color = 'red';
+      }
+    }
   });
