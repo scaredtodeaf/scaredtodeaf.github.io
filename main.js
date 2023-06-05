266997d5af6218a6ad5a2d70f11691eaee5b7b5d
scaredtodeaf <keltconor@gmail.com>
2023-06-06 01:50:07 +1000

    Redemptions page not working

--- main.js
+++ main.js
@@ -3,35 +3,17 @@ function loadPage(url) {
     pageContent.load(url);
   }
 
-  $(document).ready(function() {
-    // Function to populate the table with data
-    function populateTable(category, data) {
-      var table = $('#' + category + '-table');
-      table.empty(); // Clear previous data
-      
-      // Iterate over the data and create table rows
-      for (var i = 0; i < data.length; i++) {
-        var item = data[i];
-        var row = $('<tr></tr>');
-        var nameCell = $('<td></td>').text(item.name);
-        var textCell = $('<td></td>').text(item.text);
-        row.append(nameCell, textCell);
-        table.append(row);
-      }
-    }
+  function toggle_visibility(element) {
+    $(element).next('.sfx-group').toggle();
+  }
   
+  $(document).ready(function() {
     $('.category-list li').on('click', function() {
       var category = $(this).attr('data-category');
       $('.category-list li').removeClass('active');
       $(this).addClass('active');
       $('.table-container').removeClass('active').hide();
       $('#' + category + '-table').addClass('active').show();
-  
-      // Load data from the JSON file
-      $.getJSON('Redemptions/data.json', function(data) {
-        // Load data into the table
-        populateTable(category, data[category]);
-      });
     });
   
     $('.button').on('click', function() {
@@ -41,15 +23,21 @@ function loadPage(url) {
   
     // Hide all tables initially
     $('.table-container').hide();
-    
+  
     // Show the initial active table
     var initialCategory = $('.category-list li.active').attr('data-category');
     $('#' + initialCategory + '-table').show();
-    
-    // Load data from the JSON file for the initial active table
-    $.getJSON('data.json', function(data) {
-      // Load data into the initial active table
-      populateTable(initialCategory, data[initialCategory]);
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const sfxDivs = document.querySelectorAll('.sfx-group div[name]');
+  
+    sfxDivs.forEach(function(div) {
+      div.addEventListener('click', function(event) {
+        const name = this.getAttribute('name').replace('!', ''); // Remove the exclamation mark
+        const audio = new Audio('Redemptions/sounds/' + name + '.ogg'); // Adjust the audio file path
+        audio.play();
+      });
     });
   });
   
